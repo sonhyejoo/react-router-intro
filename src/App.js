56eaa164e4e24f856018a76dace911eb75e26517
Son Hyejoo <seongwoo.cho@gmail.com>
2023-03-27 23:17:54 +0000

    finished switch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import Home from './components/Home';
-import Stocks from './components/Stocks';
-import Movies from './components/Movies';
+import Home from "./components/Home";
+import Stocks from "./components/Stocks";
+import Movies from "./components/Movies";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className='main'>
+    <div className="main">
       <h1>App Component</h1>
-      <Home />
-      <Stocks />
-      <Movies />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/stocks">
+          <Stocks />
+        </Route>
+        <Route path="/movies">
+          <Movies />
+        </Route>
+        <Route path="/">
+          <h1>Page not found</h1>
+        </Route>
+      </Switch>
     </div>
   );
 }
